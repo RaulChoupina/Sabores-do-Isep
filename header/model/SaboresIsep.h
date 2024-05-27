c17d0896dcf3a7ad155e64217f7327d0c3207ecf
pedrocunha4 <1231690@isep.ipp.pt>
2024-05-27 20:45:59 +0100

    Update SaboresIsep.h

--- header/model/SaboresIsep.h
+++ header/model/SaboresIsep.h
@@ -5,4 +5,38 @@
 #ifndef SABORESISEP_H
 #define SABORESISEP_H
 
+#include <string>
+#include "DonoContainer.h"
+#include "ClientContainer.h"
+#include "ClientOrderContainer.h"
+#include "MenuContainer.h"
+#include "SupplierOrderContainer.h"
+#include "SupplierContainer.h"
+using namespace std;
+
+    class SaboresIsep {
+    private:
+      string name;
+      DonoContainer dono;
+      ClientContainer clients;
+      ClientOrderContainer clientorders;
+      MenuContainer menus;
+      SupplierOrderContainer supllierorders;
+      SupplierContainer Suppliers;
+
+      void setDataforConsistency();
+      public:
+      SaboresIsep();
+      SaboresIsep(const string& name);
+      string& getName();
+      void setDono( string pEmail, string pPassword);
+
+      DonoContainer & getDonoContainer();
+      ClientContainer & getClientContainer();
+      ClientOrderContainer & getClientOrderContainer();
+      MenuContainer & getMenuContainer();
+      SupplierOrderContainer & getSupplierOrderContainer();
+      SupplierContainer & getSupplierContainer();
+
+}
 #endif //SABORESISEP_H
