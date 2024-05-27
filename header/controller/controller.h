d2df660b9703546451cddcac1b0c781e39c3723c
RaulChoupina <160479758+RaulChoupina@users.noreply.github.com>
2024-05-27 20:02:16 +0100

    controller.h um pequeno erro no final preciso ver ainda

--- header/controller/controller.h
+++ header/controller/controller.h
@@ -4,5 +4,34 @@
 
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
+#include "SaboresIsep.h"
+#inlcude "View.h"
+#inlcude "ClientView.h"
+#inlcude "ClientOrderView.h"
+#inlcude "SupplierView.h"
+#inlcude "SupplierOrderView.h"
+#inlcude "ProductView.h"
+
+class controller{
+
+private:
+    SaboresIsep         model;
+    View                view;
+    ClientView          clientView;
+    ClientOrderView     clientOrderView;
+    SupplierView        supplierView;
+    SupplierOrderView   supplierOrderView;
+    ProductView         productView;
+
+    void runClients();
+    void runClientOrders();
+    void runSuppliers();
+    void runSupplierOrders();
+    void runProducts();
+public:
+    controller(SaboresIsep& saboresIsep);
+    void run();
+
+};
 
 #endif //CONTROLLER_H
