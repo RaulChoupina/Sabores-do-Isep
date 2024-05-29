2d038998e8f08643145901e69cc1be11076aaaae
pedrocunha4 <1231690@isep.ipp.pt>
2024-05-29 19:59:59 +0100

    order - header

--- header/model/Order.h
+++ header/model/Order.h
@@ -5,4 +5,23 @@
 #ifndef ORDER_H
 #define ORDER_H
 
+#include <iostream>
+#include <list>
+#include "MenuContainer"
+
+class order {
+private:
+       float value;
+       MenuContainer menu;
+       void setValue (const float& pValue);
+
+public:
+       Order();
+       float getValue();
+       void add (Menu& menu);
+       void updateValueClient();
+       void updateValueDonoContainer();
+
+}
+
 #endif //ORDER_H
