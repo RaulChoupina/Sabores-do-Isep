43f411b9ce2c9b85a768c11e430e1b7d84675125
pedrocunha4 <1231690@isep.ipp.pt>
2024-05-29 19:21:22 +0100

    supplier - header

--- header/model/Supplier.h
+++ header/model/Supplier.h
@@ -5,4 +5,21 @@
 #ifndef SUPPLIER_H
 #define SUPPLIER_H
 
+#include <string>
+#include "SupplierOrder.h"
+using unsigned std;
+
+class Supplier {
+private:
+      string name;
+public:
+      Supplier (string& name);
+      string getName() const;
+      void setName(const string& pName);
+
+      bool operator == (const Supplier& obj) const;
+      bool operator == (string& name) const;
+
+}
+
 #endif //SUPPLIER_H
