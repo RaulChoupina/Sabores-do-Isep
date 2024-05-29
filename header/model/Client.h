2b0f4a8491e4772b4cb124968b35f25847bfcc88
pedrocunha4 <1231690@isep.ipp.pt>
2024-05-29 19:03:15 +0100

    client.h (header -model)

--- header/model/Client.h
+++ header/model/Client.h
@@ -5,4 +5,36 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <string>
+#include "User.h"
+#include <list>
+
+using namespace std;
+
+class Client{
+
+private:
+      string address;
+      unsigned int nif;
+
+public:
+       Client (string &pEmail , string &pPassword);
+       Client (string &email , string &password , string &address , unsigned int nif , string &name);
+
+       Client& get();
+       void setAddress (string pAddress);
+       void setNif (unsigned int pNif);
+       string getAddress() const;
+       string getEmail() const;
+       string getName() const;
+       string getPassword() const;
+       const unsigned int getNif();
+
+       bool doesPasswordcorrect(string& password);
+
+       bool operator == (const Client& obj) const;
+       bool operator == (string& email) const;
+
+}
+
 #endif //CLIENT_H
