164a5064f478352414b5502967813673e34da90d
pedrocunha4 <1231690@isep.ipp.pt>
2024-05-29 17:50:55 +0100

    header -- user

--- header/model/User.h
+++ header/model/User.h
@@ -5,4 +5,32 @@
 #ifndef USER_H
 #define USER_H
 
+#include <string>
+#include "DuplicatedEntryException.h"
+#include "iostream"
+
+using namespace std;
+
+class User {
+
+private:
+     string name;
+     string email;
+     string password;
+
+     User ();
+     User (string& pEmail, string& pPassword);
+     void setEmail (string& pEmail);
+
+public:
+     virtualstring getEmail() const;
+     void setName (string pName);
+     void setPassword (string& pPassword);
+
+     void getPassword();
+     void getName();
+
+}
+
+
 #endif //USER_H
