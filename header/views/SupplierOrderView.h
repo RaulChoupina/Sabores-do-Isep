0f4f35a48574cdfaea4df267d1103593debfe51c
pedrocunha4 <1231690@isep.ipp.pt>
2024-06-03 20:26:14 +0100

    vou acabar, vou so jantar

--- header/views/SupplierOrderView.h
+++ header/views/SupplierOrderView.h
@@ -5,4 +5,13 @@
 #ifndef SUPPLIERORDERVIEW_H
 #define SUPPLIERORDERVIEW_H
 
+#include "SupplierOrder.h"
+#include "View.h"
+#include "SupplierOrderContainer.h"
+
+        class SupplierOrder{
+        
+
+}
+
 #endif //SUPPLIERORDERVIEW_H
