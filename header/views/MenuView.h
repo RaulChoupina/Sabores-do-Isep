ef5799000602b6455a30134b1390095482335703
pedrocunha4 <1231690@isep.ipp.pt>
2024-06-03 16:12:08 +0100

    Update MenuView.h

--- header/views/MenuView.h
+++ header/views/MenuView.h
@@ -5,4 +5,17 @@
 #ifndef MENUVIEW_H
 #define MENUVIEW_H
 
+#include "Menu.h"
+#include "View.h"
+
+      class MenuView{
+      public:
+      Menu getMenu (const string& name);
+      void printMenuClient (Menu & menu);
+      void printMenuContainerClient (container < Menu >& menu, const string& containerTitle);
+      void printMenuContainerDono (container < Menu >& menu, const string& containerTitle);
+      string getName
+      void MenuUnavailable();
+      }
+      
 #endif //MENUVIEW_H
