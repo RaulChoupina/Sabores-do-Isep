0849344ae3e8fb75153a654906604bbe6579f988
1230643 <1230643@isep.ipp.pt>
2024-06-05 00:13:39 +0100

    ClientOrder

--- sources/model/ClientContainer.cpp
+++ sources/model/ClientContainer.cpp
@@ -1,3 +1,14 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "ClientContainer.h"
+
+Container<Client> :: iterator ClientContainer:: searchByEmail(string &Email){
+    auto it =this->clients.begin();
+    for(; it!= this->clients.end(); ++it){
+        if((*it)== Email){
+            return it;
+        }
+    }
+    return it;
+}
--- sources/model/ClientOrder.cpp
+++ sources/model/ClientOrder.cpp
@@ -1,3 +1,28 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "ClientOrder.h"
+#include "Utils.h"
+#include "InvalidDataException.h"
+
+ClientOrder :: ClientOrder (Date date, Client* client,float value){
+    this-> date=date;
+    this->setClient(client);
+    this->setValue(value);
+}
+void ClientOrder:: setClient(client *pClient){
+    if(pClient != nullptr){
+        this->client = pClient;
+    }else{
+        this ->client =nullptr;
+        string msg = "ClientOrder : Client =Null";
+        throw InvalidDataException(msg)
+    }
+}
+
+Client * ClientOrder:: getClient(){
+    return this -> Client;
+}
+ClientOrder &ClientOrder:: get(){
+    return *this;
+}
