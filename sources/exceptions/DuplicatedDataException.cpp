c9de6798162af8fcffbfe745d6bc05eacb0af1cf
1230643 <1230643@isep.ipp.pt>
2024-06-04 19:57:48 +0100

    DuplicatedDataException

--- sources/exceptions/DuplicatedDataException.cpp
+++ sources/exceptions/DuplicatedDataException.cpp
@@ -1,3 +1,12 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "../header/DuplicateEntryException.h"
+
+DuplicateEntryException :: DuplicateEntryException(string data){
+    this ->data = "Error: ["+data +"] duplicated !!";
+}
+
+const char* DuplicatedEntryException :: what (){
+    return this -> data.c_str();
+}
