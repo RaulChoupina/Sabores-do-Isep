89b601ed6bdcdc2dc47a67997deacdd73e4524e9
pedrocunha4 <1231690@isep.ipp.pt>
2024-06-05 19:27:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "SaboresIsep.h"
+#include "mock/MockData.h"
+#include "Controller.h"
+
+using namespace std;
 
 int main() {
+    SaboresIsep saboresIsep("Restaurante");
+    saboresIsep.setDono("dono");
 
+    MockData mock;
+    mock.generateData(saboresIsep);
+
+    Controller controller(saboresIsep);
+    controller.run();
+    cout << "Application has exited" << endl;
     return 0;
 }
