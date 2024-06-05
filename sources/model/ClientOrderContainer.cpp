0e55babb9b350f11829e0abb4e9af7dcc6178c9f
1230643 <1230643@isep.ipp.pt>
2024-06-05 17:46:03 +0100

    ClientOrderContainer

--- sources/model/ClientOrderContainer.cpp
+++ sources/model/ClientOrderContainer.cpp
@@ -1,3 +1,54 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "ClientOrderContainer.h"
+
+ClientOrderContainer::ClientOrderContainer(){
+
+}
+void  ClientOrderContainer::add(const ClientOrder &pClientOrder){
+    this ->clientOrders.push_back(pClientOrder);
+}
+void ClientOrderContainer:: setClients (ClientContainer *clients){
+    this->clients=clients
+}
+void ClientOrderContainer:: setMenu(MenuContainer *Menu){
+    this-> Menu = Menu
+}
+ ClientOrderContainer::getClientOrders(){
+    return{};
+}
+ClientOrderContainer:: getClientOrdersByClient(Client *pClient,ClientOrderContainer& modelClientOrderContainer){
+    return ClientOrderContainer:
+    auto it = modelClientOrderContainer.ClientOrders.begin();
+    for (; it != modelClientOrderContainer.clientOrders.end(); ++it){
+        if((it->getClient())== pClient){
+            ClientOrder tempClientOrder = it->get();
+            return ClientOrderContainer.add(tempClientOrder);
+        }
+    }
+    return ClientOrderContainer;
+}
+Container<ClientContainer> ClientOrderContainer:: getALL(){
+    Container<ClientOrder> newClientOrderContainer (this->ClientOrder);
+    return newClientOrderContainer;
+}
+bool ClientOrderContainer:: hasClientMadeOrders (client *pClient, CleintOrderContainer &modelClientOrderContainer){
+    bool areThereOrders = false
+            auto it = modelClientOrderContainer.ClientOrders.begin();
+    for (; it !=this-> clientsOrders.end(); ++it){
+        if((it->getClient())== pClient){
+            areThereOrders = true;
+        }
+    }
+    return areThereOrders;
+}
+
+int clientOrderContainer::getSize(ClientOrderList &ClientOrderContainer){
+    int i = 0;
+    auto it = ClientOrderContainer.ClientOrders.begin();
+    for(;it != ClientOrderContainer.ClientOrders.end();++it){
+        ++i;
+    }
+    return i;
+}
