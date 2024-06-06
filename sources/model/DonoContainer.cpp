bffe82d8949fbae8a50820c790ecf25d3651a5c0
1230643 <1230643@isep.ipp.pt>
2024-06-06 16:42:18 +0100

    DonoContainer

--- sources/model/DonoContainer.cpp
+++ sources/model/DonoContainer.cpp
@@ -1,3 +1,9 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "DonoContainer.h"
+
+void Dono:: set(string &pEmail, string &pPassword){
+    this -> Email = pEmail;
+    this -> Password = pPassword;
+}
