67b393fa81f541c889c390686d509a6d24057b91
1230643 <1230643@isep.ipp.pt>
2024-06-06 22:20:49 +0100

    SupplierOrderContainer

--- sources/model/SupplierOrderContainer.cpp
+++ sources/model/SupplierOrderContainer.cpp
@@ -1,3 +1,42 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "SupplierOrderContainer.h"
+
+SupplierOrder *SupplierOrderContainer::add (const unsigned int &SupplierOrderID){
+    return nullptr;
+}
+void SupplierOrderContainer::add(const SupplierOrder& pSupplierOrder){
+    this ->supplierOrders.push_back(pSupplierOrder);
+}
+void SupplierOrderContainer:: setSuppliers (SupplierContainer *pSuppliers){
+    this -> suppliers =pSuppliers;
+}
+
+int SupplierOrderContainer::getSize(SupplierOrderContainer &supplierOrderContainer){
+    int i =0;
+    auto it = supplierOrderContainer.SupplierOrders.begin();
+    for (; it!= SupplierOrderContainer.SupplierOrders.end(); ++it){
+        ++i;
+    }
+    return i;
+}
+SupplierOrderContainer::SupplierOrderContainer(){
+
+}
+Container<SupplierOrder> SupplierOrderContainer::getAll(){
+    Container<SupplierOrder>newSupplierOrderContainer (this-> supplierOrders)
+    return newSupplierOrdersContainer;
+}
+SupplierOrderContainer
+SupplierOrderContainer::getSupplierOrdersBySupplier(Supplier *pSupplier, SupplierOrders &modelSupplierOrderContainer){
+    SupplierOrderContainer returnSupplierOrderContainer;
+    auto it = modelSupplierOrderContainer.SupplierOrders.begin();
+    for (;it != modelSupplierOrderContrainer.SupplierOrders.end(); ++it){
+        if((it->getSupplier())== pSupplier){
+            SupplierOrder tempSupplierOrder = it->get();
+            returnSupplierOrderContainer.add(tempSupplierOrder);
+        }
+    }
+    return SupplierOrderContainer;
+}
