8c3bb5c8a2a6a5b867765c3915687c3823bcc288
1230643 <1230643@isep.ipp.pt>
2024-06-06 21:15:40 +0100

    SupplierOrder

--- sources/model/SupplierOrder.cpp
+++ sources/model/SupplierOrder.cpp
@@ -1,3 +1,25 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "supplierOrder.h"
+#include "Utils.h"
+
+SupplierOrder:: SupplierOrder (Date date, Supplier *pSupplier,float value){
+    this->date =date;
+    this-> setSupplier(pSupplier);
+    this-> setValue(value);
+}
+void SupplierOrder:: setSupplier (supplier *pSupplier){
+    if (psupplier !=nullptr){
+        this->supplier =pSupplier;
+    }else{
+        string msg = "Supplier = NULL";
+        throw InvalidDataException(msg);
+    }
+}
+Supplier *SupplierOrder::getSupplier(){
+    return this->supplier;
+}
+SupplierOrder &SupplierOrder::get(){
+    return *this
+}
