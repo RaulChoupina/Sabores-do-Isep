8e4602dfcde9a1f127ffffe28d97c228a9e70587
1230643 <1230643@isep.ipp.pt>
2024-06-06 17:27:30 +0100

    Order

--- sources/model/Order.cpp
+++ sources/model/Order.cpp
@@ -1,3 +1,28 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "Order.h"
+
+Order::Order(){
+    setValue (0);
+}
+float Order::getValue(){
+    return this ->value
+}
+void Order:: setValue (cont float &pValue){
+    this ->value =pValue;
+}
+
+void Order:: add(Menu &menu){
+    this ->value = pValue;
+}
+
+string Order:: getDate(){
+    return this -> date.getDate();
+}
+void Order:: updatedValueClient(){
+    float tempValue = 0;
+    Container<Menu> tempMenu = this ->Menu.getAll();
+    auto it = tempMenu.begin();
+    value =tempValue;
+}
