f4c4e222480174a872ae9a3d3a42a91f01af8611
1230643 <1230643@isep.ipp.pt>
2024-06-06 18:05:46 +0100

    Supplier

--- sources/model/Supplier.cpp
+++ sources/model/Supplier.cpp
@@ -1,3 +1,52 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "Supplier.h
+
+Supplier:: Supplier (string& name,bool isActive){
+    setName(name);
+    if(isActive){
+        setActive();
+    }else{
+        setInactive();
+    }
+}
+
+string Supplier:: getName() const{
+    return name;
+}
+
+string Supplier:: getState() const{
+    if(this ->isActive)
+        return "Available";
+    return "Unavailable";
+}
+
+void Supplier:: toggleState(){
+    if (this-> isActive){
+        this->isActive = false;
+    }else{
+        this -> isActive =true;
+    }
+}
+void Supplier:: setActive(){
+    this ->isActive = true;
+}
+void Supplier:: setInactive(){
+    this->isActive =false;
+}
+void Supplier:: setName( const string &pName){
+    this-> name =pName
+}
+
+bool Supplier:: operator== (const Supplier &obj) const{
+    if(this ->getName()== obj.getName()){
+        return true;
+    }return false;
+}
+
+bool Supllier:: operator == (string &name) const{
+    if (this->name == name){
+        return true;
+    }return false;
+}
