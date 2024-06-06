6fd3a3ec32daef1e9f9c495f82c3a237faaaa48f
1230643 <1230643@isep.ipp.pt>
2024-06-06 18:48:34 +0100

    SupplierContainer

--- sources/model/SupplierContainer.cpp
+++ sources/model/SupplierContainer.cpp
@@ -1,3 +1,50 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "SupplierContainer.h
+
+Container<Supplier> SupplierContainer:getAll() {
+    Container <Supplier> SupplierContainer(this->suppliers);
+    return newSupplierContainer;
+}
+Container<Supplier>:: iterator SupplierContainer::searchByName (string &name) {
+    auto it = this->suppliers.begin();
+    for (; it != this->suppliers.end(); ++i) {
+        if ((*it) == name) {
+            return it;
+        }
+    }
+    return it;
+}
+Supplier *SupplierContainer::getByName(string& name){
+    auto it = searchByName(name);
+    if (it != this ->suppliers.end()){
+        return &(*it);
+    }
+    return nullptr;
+}
+void SupplierContainer:: addSupplier(const Supplier &supplier){
+    string name = supplier.getName();
+    auto it = searchByName (name);
+    if (it == this ->suppliers.end()){
+        this->suppliers.push_back(supplier);
+    }else{
+        string msg ="Supplier:"+ name;
+        throw DuplicateDataException(msg);
+    }
+}
+void SupplierContainer::toggleState (string& name){
+    auto it =searchByName (name);
+    if (it == this->suppliers.end()){
+        this -> suppliers.push_back(supplier);
+    }else{
+        string msg ="Supplier:" + name;
+        throw DuplicateDataException (msg);
+    }
+}
+void SupplierContainer:: toogleState(string& name){
+    auto it = searchByName(name);
+    if(it != this-> suppliers.end()){
+        it->toggleState();
+    }
+}
