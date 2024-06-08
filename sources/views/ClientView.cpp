dcb67e872bcbb9c084a133ef95843ea9f7f49aaf
pedrocunha4 <1231690@isep.ipp.pt>
2024-06-08 18:52:41 +0100

    Update ClientView.cpp

--- sources/views/ClientView.cpp
+++ sources/views/ClientView.cpp
@@ -1,3 +1,82 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include <iostream>
+#include "ClientView.h"
+#include "Utils.h"
+#include "InvalidDataException.h"
+#include "View.h"
+
+using namespace std;
+
+string ClientView :: getEmail(){
+    return Utils:: getString ("Please enter your email")
+}
+string ClientView :: getPassword(){
+    return Utils:: getString ("Please enter your password")
+}
+string ClientView :: invalidEmail(){
+    cout << "Invalid Email";
+}
+string ClientView :: incorrectPassword(){
+    cout << "Incorrect Password";
+}
+void printClientContainerDono(list<Client>& clients, const string& containerTitle){
+    cout << containerTitle << endl;
+    cout << left << setw(NAMESPACING) << "Name";
+    cout << left << setw(EMAILPACING) << "Email";
+    cout << left << setw(ADDRESSSPACING) << "Address";
+    cout << left << setw(NIFSPACING) << "Nif";
+    cout << endl;
+
+    for (Client client : clients]{
+    cout << containerTitle << endl;
+    cout << left << setw(NAMESPACING) << client.getName();
+    cout << left << setw(EMAILPACING) <<  client.getEmail();
+    cout << left << setw(ADDRESSSPACING) <<  client.getAddress();
+    cout << left << setw(NIFSPACING) <<  client.getNif();
+    cout << endl;
+    }
+}
+string ClientView:: getName(){
+string name;
+int spaceCount;
+    do{
+    spaceCount = 0;
+        name = Utils::getString("Please enter your first and last name");
+        for(int i = 0; name[i] != '\0'; i++){
+            if(name[i] == ' ')
+                spaceCount++;
+        }
+        if(spaceCount == 0)
+            cout << "Too few words" << endl;
+        if(spaceCount > 1)
+            cout << "Too many words" << endl;
+    }while(spaceCount !=1);
+    return  name;
+}
+string ClientView:: getAddress(){
+      bool isValidAddress;
+      string number, address, street name;
+   do{
+      address = Utils::getString("Please enter your address (street name and number)");
+      size_t pos = address.find.first_of(',');
+      number = address.substr (0,pos);
+      if(!Utils::stringIsNumber(number)){
+        isValidAddress = false;
+      }else{
+        isValidAddress = true;
+      }
+   }while(!isValidAddress);
+   return address;
+}
+unsigned int ClientView:: getNif(){
+    unsigned int nif;
+    do{
+         nif = Utils::getUInt("Please enter your nif");
+    }while( nif < 100000000 || nif > 999999999);
+    return nif;
+}
+void ClientView::ClientNotFound(){
+    cout << "Client not found";
+}
