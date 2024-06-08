3cb23fe5aa6630db541de62eac75e4b64cf2f909
1230643 <1230643@isep.ipp.pt>
2024-06-08 22:36:53 +0100

    User

--- sources/model/User.cpp
+++ sources/model/User.cpp
@@ -1,3 +1,29 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include "User.h"
+#include <utility>
+
+using namespace std;
+
+User::User (string& pEmail, string& pPassword){
+    setEmail (pEmail);
+    setPassword (pPassword);
+}
+
+String User ::getEmail() const {
+    return this-> username;
+}
+
+string User ::getPassword() {
+    return this-> password;
+}
+void User :: setPassword (string &pPassword){
+    this-> password =pPassword;
+}
+void User::setEmail(string &pEmail) {
+    this->username =pUsername
+}
+User::User(){
+
+}
