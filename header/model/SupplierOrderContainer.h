5b03ceb56748fc527ff637ca2541f149f7b910cb
pedrocunha4 <1231690@isep.ipp.pt>
2024-06-13 23:48:41 +0100

    Update SupplierOrderContainer.h

--- header/model/SupplierOrderContainer.h
+++ header/model/SupplierOrderContainer.h
@@ -5,4 +5,26 @@
 #ifndef SUPPLIERORDERCONTAINER_H
 #define SUPPLIERORDERCONTAINER_H
 
+#include <iostream>
+#include "SupplierContainer.h"
+#include "SupplierOrder.h"
+#include "MenuContainer.h"
+
+using namespace std;
+
+class SupplierOrderContainer{
+private:
+      container < SupplierOrder > supplierOrders;
+      SupplierContainer* suppliers;
+      MenuContainer* menus;
+public:
+      SupplierOrderContainer();
+      SupplierOrder* get(const unsigned int& supplierOrderID);
+      int getSize(SupplierOrderContainer& supplierOrderContainer);
+      void add(const SupplierOrder& pSupplierOrder);
+      void setMenus(MenuContainer& pMenus);
+      void setSuppliers(SupplierContainer& pSuppliers);
+      container < SupplierOrder > getAll();
+      SupplierOrderContainer getSupplierOrdersBySupplier(Supplier* pSupplier, SupplierOrderContainer& modelSupplierOrderContainer);
+}
 #endif //SUPPLIERORDERCONTAINER_H
