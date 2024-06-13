0021dfc406f763a37aab691348681949fc95b81f
pedrocunha4 <1231690@isep.ipp.pt>
2024-06-13 19:15:26 +0100

    Mock

--- mock/MockData.cpp
+++ mock/MockData.cpp
@@ -1,3 +1,106 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include <iostream>
+#include "MockData.h"
+
+void MockData:: loadMockClients (ClientContainer& clientContainer){
+    int rows = 100;
+    int cols = 5;
+    string data[rows][cols];
+
+    ifstream file("../data/mockClients.csv");
+
+    if (!file.is_open()) {
+        cout << "File not opened" << endl;
+        return;
+    }
+
+    for(int row = 0; row < rows; ++row)
+    {
+        string line;
+        getline(file, line);
+
+        stringstream iss(line);
+
+        for (int col = 0; col < columns; ++col)
+        {
+            string val;
+            getline(iss, val, ',');
+            data[row][col] = val;
+        }
+    }
+    for(int i = 1; i < rows; ++i){
+        Client client(data[i][0], data[i][1], data[i][2], data[i][3], stoi(data[i][4]));
+        clientContainer.addClient(client);
+    }
+}
+void MockData::generateData(SaboresIsep& saboresIsep){
+     loadMockClients (SaboresIsep.getClientContainer());
+     loadMockMenu (SaboresIsep.getMenuContainer());
+     loadMockSupplier (SaboresIsep.getSupplierContainer());
+}
+void MockData:: loadMockMenu (MenuContainer& menuContainer){
+     int rows = 10;
+     int cols = 6;
+     string data[rows][cols];
+
+     ifstream file("../data/mockClients.csv");
+
+     if (!file.is_open()) {
+        cout << "File not opened" << endl;
+        return;
+     }
+     for (int rows = 0; row < rows; ++rows){
+     string val;
+     getline(iss,val, ';');
+     data[row][col] = val;
+     }
+}
+for(int i = 1; i < rows; ++i){
+    replace(data[i][4].begin(), data[i][4].end(), ',','.');
+    replace(data[i][5].begin(), data[i][5].end(), ',','.');
+
+    bool available = stoi(data[i][8]) ;
+    Product product(data[i][0],
+                    data[i][1],
+                    data[i][2],
+                    stoi(data[i][3]),
+                    stof(data[i][4]),
+                    stof(data[i][5]);
+    menuContainer.add(product);
+    }
+}
+void MockData:: loadMockSuppliers (SupplierContainer& supplierContainer){
+    int rows = 10;
+    int cols = 2;
+    string data[rows][cols];
+
+    ifstream file("../data/mockSuppliers.csv");
+
+    if (!file.is_open()) {
+        cout << "File not opened" << endl;
+        return;
+    }
+    for(int row = 0; row < rows; ++row)
+    {
+        string line;
+        getline(file, line);
+
+        stringstream iss(line);
+
+        for (int col = 0; col < cols; ++col)
+        {
+            string val;
+            getline(iss, val, ',');
+            data[row][col] = val;
+        }
+    }
+    for(int i = 1; i < rows; ++i){
+        bool available = stoi(data[i][1]) ;
+        Supplier supplier(data[i][0], available);
+        supplierContainer.addSupplier(supplier);
+    }
+}
+
+
--- mock/MockData.h
+++ mock/MockData.h
@@ -5,4 +5,22 @@
 #ifndef MOCKDATA_H
 #define MOCKDATA_H
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "SaboresIsep"
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+class MockData(){
+private:
+     void loadMockClients(ClientContainer& clientContainer);
+     void loadMockMenu(MenuContainer& menuContainer);
+     void loadMockSuppliers(SupplierContainer& supplierContainer);
+public:
+     void generateData(SaboresIsep& saboresIsep);
+}
+
 #endif //MOCKDATA_H
