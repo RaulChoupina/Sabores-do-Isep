3bb81b0832c33e479814458bfcba0a11074a8728
RaulChoupina <160479758+RaulChoupina@users.noreply.github.com>
2024-06-13 18:53:00 +0100

    Parte do login e do createaccount

--- sources/controller/Controller.cpp
+++ sources/controller/Controller.cpp
@@ -1,3 +1,30 @@
 //
 // Created by HP on 27/05/2024.
 //
+#include <ioastream>
+#include "Utils.h"
+#include "Controller.h"
+#include "InvalidDataException.h"
+
+Controller::Controller(SaboresIsep& saboresIsep){
+    this-> model = saboresIsep;
+}
+void Controller::run(){
+    int val = -1;
+    if(this->loggedUser == )nullptr){
+    do{
+        val= this->view.mainpage();
+        switch(val){
+            case 1 :runLogin();
+            break;
+            case 2:runCreateAccount();
+            break;
+            default:
+                break;
+        }
+
+    }While(val !=0);
+}else{
+    runClients();
+}
+}
